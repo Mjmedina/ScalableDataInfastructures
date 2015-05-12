7f60db814df3c3b8b85c709f17c6fa975fe6a5a9
Mjmedina <mjmedina@fullsail.edu>
2015-05-12 08:55:44 -0400

    Day 3 Lab
    Work for day 3 lab

---
+++ Medina_Michael_ScalableDataInfastructures/Day 2 Lab/js/Medina_Michael_lab2.js
@@ -0,0 +1,21 @@
+//alert("JavaScript works!");
+
+//I want to play a game from my favorite franchise, Super Mario.
+var myFavoriteGame = "Super Mario";
+
+//The two options I want to pick between are playing on N64 or GBA.
+var bestConsole = " Nintendo \"64\"";
+var worstConsole = "Gameboy Advanced";
+
+
+//How many hours do I have to play?
+var userInput = prompt("How many hours do you have free to play?");
+
+//Choose a game that I want to play
+console.log("I want to play"+myFavoriteGame+".");
+
+//Choose which console to play on and hours available to play
+console.log ("What should I play on?" + bestConsole + " or "+ worstConsole);
+console.log(userInput);
+console.log("I have " + userInput+" hours to play today");
+
---
+++ Medina_Michael_ScalableDataInfastructures/Day Number 2/js/DayNumber2.js
@@ -0,0 +1,31 @@
+
+/*
+ Michael Medina
+ Date
+ Section 00
+ Assignment
+ */
+
+//alert("Just testing!");
+
+
+// Single-Line Comment
+
+//My Variables. //Grouped variables for ease of view.
+
+var myHomeAddress   = "2361 Verde Lane";
+var milesToStore    =  8.5;
+//var construction    = "false";
+var myFavoriteStore = "Publix Supermarket";
+
+
+//Confirm
+var construction = confirm("Is there construction? Click OK for yes, Cancel for no.");
+
+//Output
+
+console.log("I live at "+ myHomeAddress + ".");
+console.log("I shop at " + myFavoriteStore + ".");
+console.log ("It's only " + milesToStore + " to the store");
+console.log ("It's " + construction + " that there is construction enroute.");
+
