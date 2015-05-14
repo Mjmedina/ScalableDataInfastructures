2ad974d627bba3f5eb9a888eb87748384527b56b
Mjmedina <mjmedina@fullsail.edu>
2015-05-14 15:26:19 -0400

    Calculator
    Functions

--- Medina_Michael_ScalableDataInfastructures/SDI Lab 5/js/Medina_Michael_lab5.js
+++ Medina_Michael_ScalableDataInfastructures/SDI Lab 5/js/Medina_Michael_lab5.js
@@ -1,4 +1,4 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 
 function addNumbers (a, b)
 {
@@ -14,4 +14,37 @@ function multNumbers (a, b)
     return a *= b;
 }
 
+
+function divNumbers (a, b){
+    while (a=== 0 || b=== 0) {
+
+        console.log ("Enter new values that aren't 0 when dividing.");
+        a = parseInt(prompt("Please insert the first number"));
+        b = parseInt(prompt("Please insert the second number"));
+    }  return a /= b;
+
+
+
+}
+
+var firstNum = parseInt(prompt("Please insert the first number"));
+var secondNum = parseInt(prompt("Please insert the second number"));
+var operation = "0";
+
+while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+{
+    operation = prompt("Please enter an operator.");
+} if (operation === "+"){
+    var final = addNumbers(firstNum, secondNum)
+} else if (operation === "-"){
+    var final = subNumbers(firstNum, secondNum)
+} else if (operation === "*"){
+    var final = multNumbers(firstNum, secondNum)
+} else if (operation === "/"){
+    var final = divNumbers(firstNum, secondNum)
+} else {
+    console.log("This is not what i asked for. ")
+}
+
+console.log (firstNum +" " + operation + " " + secondNum + " = "+ final);
 
