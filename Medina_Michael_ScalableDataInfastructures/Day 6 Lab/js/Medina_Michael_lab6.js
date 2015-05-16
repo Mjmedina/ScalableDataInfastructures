965599326cc8036c9d488049fbbb28ba94e5fe69
Mjmedina <mjmedina@fullsail.edu>
2015-05-16 14:25:04 -0400

    create for loop
    for loop

--- Medina_Michael_ScalableDataInfastructures/Day 6 Lab/js/Medina_Michael_lab6.js
+++ Medina_Michael_ScalableDataInfastructures/Day 6 Lab/js/Medina_Michael_lab6.js
@@ -7,8 +7,14 @@
 //create a fucntion to place array inside of.
 function theAvengers() {
     var avengerNames = ["Iron Man", "Black Widow", "Thor", "Hulk"];
-    var secretIdentity = ["Tony Stark", "Natasha Romanov", ".... uhhh THOR!?", "Bruce Banner"];}
+    var secretIdentity = ["Tony Stark", "Natasha Romanov", ".... uhhh THOR!?", "Bruce Banner"];
 
+for (var i = 0; i < avengerNames.length; i++) {
+    {
+        console.log(avengerNames[i] + "'s secret identity is " + secretIdentity[i]);
+
+    }
+}
 
 
 
